550b84cda0744188ea027ea7cb4b141a31f695a9
sanamyea <91358685+sanamyea@users.noreply.github.com>
2023-08-09 14:19:35 +1000

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,24 @@
+package human;
+
+public class Human {
+
+		private int rateofRespiration;
+		public Human(int rateofRespiration) {
+			this.rateofRespiration = rateofRespiration;
+			
+		}
+		public int getRateofRespiration() {
+			return rateofRespiration;
+		}
+		public void setRateofRespiration(int rateofRespiration) {
+			this.rateofRespiration=rateofRespiration;
+			
+		}
+		
+		public static void main(String[] args) {
+		System.out.println("Hello World ");
+		}
+		
+	}
+
+
